04f18f188c277897260359c6414751b926eb0570
Sneha2125 <36706339+Sneha2125@users.noreply.github.com>
2020-07-14 23:54:41 +0530

    Create Hello.cs

---
+++ Hello.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Build_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome to Build Application");
+            Console.ReadLine();
+
+        }
+    }
+}
